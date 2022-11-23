cc96879e5cb2f48cc0d5c65e99b350e93ec414ad
Phoenix Pulsar <phoenixpulsar@protonmail.com>
2022-11-22 18:45:58 -0700

    Added nvim and visual studio code configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,12 @@
 require("themacedo.set")
 require("themacedo.remap")
-require("treesitter-config")
+-- require("treesitter-config")
 require("telescope-config")
+require("cmp-config")
+require("autopairs-config")
 
 local function blah()
   print "Welcome to the Machine\n"
 end
+
 
--- nvim/.config/nvim/lua/themacedo/packer.lua
+++ nvim/.config/nvim/lua/themacedo/packer.lua
@@ -1,5 +1,10 @@
 vim.cmd [[packadd packer.nvim]]
 
+-------------IMPORTANT----------------------
+-- After adding a plugin always remember to
+-- source so % and then run PackerInstall
+-- -----------------------------------------
+
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -11,20 +16,33 @@ return require('packer').startup(function(use)
     -- either with a range like :7,17Commentary, or as part of a :global
     -- invocation like with :g/TODO/Commentary. That's it.
   use 'tpope/vim-commentary'
-  -- Git Fugitive 
+  -- Git Vim Fugitive 
   use 'tpope/vim-fugitive'
+  -- Vsnip Snipets
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  use 'hrsh7th/vim-vsnip'
+  use 'hrsh7th/vim-vsnip-integ'
+  -- AutoPairs
+  use {
+	"windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
   -- Theme
   use 'arcticicestudio/nord-vim'
-  --  Comment out shaunsingh/nord for now using 
-  --  nord-vim above, made text italic
-  -- use 'shaunsingh/nord.nvim'
-  -- Syntax Highlights
-  use 'nvim-treesitter/nvim-treesitter'
   -- Telescope
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-
-
+  -- Syntax Highlights
+  -- use 'nvim-treesitter/nvim-treesitter'
+  -- stop working commented out, doesnt look like we need it though
+  -- leaving in case we want to find new way to add syntax
+  -- use 'nvim-treesitter/nvim-treesitter'
 end)
